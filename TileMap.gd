d5cb02aa0df67d3be1cdc692aa18a404286c80fb
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-02-03 21:27:27 +0100

    Add files via upload

---
+++ TileMap.gd
@@ -0,0 +1,28 @@
+extends TileMap
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	var cnt=0
+#	for j in range(100):
+#		for i in range(100):
+#			var c = get_cell(i,j)
+#			var d = get_cell_autotile_coord(i,j)
+#			set_cell(i,j,0,false,false,false,d)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+#func _get_subtile_coord(id):
+#    var tiles = $TileMap.tile_set
+#    var rect = tilemap.tile_set.tile_get_region(id)
+#    var x = randi() % int(rect.size.x / tiles.autotile_get_size(id).x)
+#    var y = randi() % int(rect.size.y / tiles.autotile_get_size(id).y)
+#    return Vector2(x, y)

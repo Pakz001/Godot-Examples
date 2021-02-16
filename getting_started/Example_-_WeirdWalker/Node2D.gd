1ff6e8af03d47a88a94f9b2a5f7f37f364ef1186
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-02-16 22:23:16 +0100

    Add files via upload

--- getting_started/Example_-_WeirdWalker/Node2D.gd
+++ getting_started/Example_-_WeirdWalker/Node2D.gd
@@ -8,6 +8,7 @@ var map = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	# first we create our map array[][]
 	for x in range(0,20):
 		map.append([])
@@ -22,32 +23,50 @@ func _ready():
 	map[x][y]=1
 	# when we get a high countout we then backtrack our path
 	var countout = 0
+	# olddir and oldval holds our previous direction(for longer hallways)
+	var olddir
+	var oldval
+	var start=true
 	# We want to loop enough to fill the map(20*20 cells)
-	for i in range(0,20*20*10):
+	for i in range(0,20*20*8):
+		
 		# oldx/y holds our position we can use to reset to if we fail our current step
 		var oldx = x
 		var oldy = y
 		# stepx/y checks one step ahead
 		var stepx = x
 		var stepy = y
-		# direction 1 is horizontal and 2 is vertical
-		var dir=10
+		# direction 1 is horizontal and 2 is vertical		
+		var dir
 		# val holds our direction -1 or 1
 		var val
-		if(rand_range(0,10)<5):
-			val=-1
-		else:
-			val=1
+
+		# we first restore our previous direction
+		val = oldval
+		dir = olddir
+		
+		# if we are in the first step or when we want to head into a random direction
+		if(start==true || rand_range(0,10)<5):
+			start=false
+			if(rand_range(0,10)<5):
+				val=-1
+			else:
+				val=1
+			if(rand_range(0,10)<5):
+				dir=1
+			else:
+				dir=2
+
 		# Chose if we want to move horizontal or vertical	
-		if(rand_range(0,10)<5):			
-			dir=1
+		if(dir==1):			
 			x+=val
 			stepx +=(val*2)
 		else:			
-			dir=2
 			y+=val
 			stepy += (val*2)
-		# variable go is used to cancel or go ahead with our path
+			
+			
+		# variable go is used to cancel or go ahead with our path		
 		var go = true
 		
 		# if we step outside of the map then restore position and cancel the current step
@@ -62,17 +81,28 @@ func _ready():
 		# if our step ahead inside the map is already taken than fail!
 		if(map[stepx][stepy]==1):
 			go=false
-		# if our direction is horizontal check above and below/left and right if nothing is there yet(fail!)		
+		# if our direction is horizontal check above and below/left and right if nothing is there yet(fail!)	
+		# also check if our step ahead has no walkables above/left/right/down of it.	
 		if(dir==1):
 			if(y-1>=0 && map[x][y-1]==1):
 				go=false
 			if(y+1<20 && map[x][y+1]==1):
 				go=false				
+			if(stepy-1>=0 && map[stepx][stepy-1]==1):
+				go=false
+			if(stepy+1<20 && map[stepx][stepy+1]==1):
+				go=false				
+
 		if(dir==2):
 			if(x-1>=0 && map[x-1][y]==1):
 				go=false
 			if(x+1<20 && map[x+1][y]==1):
 				go=false				
+			if(stepx-1>=0 && map[stepx-1][stepy]==1):
+				go=false
+			if(stepx+1<20 && map[stepx+1][stepy]==1):
+				go=false				
+
 		# if nothing is wrong then push the current position to the array
 		# and put it on the map. Reset the countout since we succeeded.
 		# if we failed then restore our old position and increase our 
@@ -81,9 +111,13 @@ func _ready():
 			array.push_front(Vector2(x,y))
 			map[x][y]=1
 			countout=0
+			olddir = dir
+			oldval = val
 		else:
 			x = oldx
 			y = oldy
+			olddir = dir
+			oldval = val
 			countout+=1
 		# if our backtracking counter is above 5 then reset this counter
 		# and backtrack on our path and put the previous position in the
